373336352cdc3efdbf8d01b670b8561cb3eda275
pratikbanjare <jay.lelouch@outlook.com>
2018-07-16 10:10:28 +0530

    compass

---
+++ Assets/Compass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Compass : MonoBehaviour {
+
+
+    GameObject Player;
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<RawImage>().uvRect = new Rect(Player.transform.localEulerAngles.y / 360, 0, 1, 1);
+	}
+}
